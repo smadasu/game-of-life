66b8f7ab0678d880675d43bf878b495eab63b7d0
Sreenath Madasu <sreenath.madasu@gmail.com>
2015-02-22 14:26:11 -0500

    More clean up

--- desktop/src-common/game_of_life/core.clj
+++ desktop/src-common/game_of_life/core.clj
@@ -12,11 +12,11 @@
 (def initial-state 
   (->> co-ordinates 
        (map (fn [[x y]]
-         (let [alive? (even? (rand-int 100))
-               x-of-region (if alive? 0 pixel-size)]
-           (assoc (texture "combined.jpeg" :set-region x-of-region 0 pixel-size pixel-size)
-                  :x x :y y :width pixel-size :height pixel-size 
-                  :isCell? true :alive? alive?))))))
+              (let [alive? (even? (rand-int 100))
+                    x-of-region (if alive? 0 pixel-size)]
+                (assoc (texture "combined.jpeg" :set-region x-of-region 0 pixel-size pixel-size)
+                       :x x :y y :width pixel-size :height pixel-size 
+                       :isCell? true :alive? alive?))))))
 
 (def neighbors
   (into {} (map (fn [[x y]]
@@ -27,52 +27,47 @@
                              (list (- y pixel-size) y (+ y pixel-size))))}) co-ordinates)))
 
 (defn- change-entity-state [entity live-state]
-    (texture! entity
-              :set-region (if live-state 0 pixel-size) 0 pixel-size pixel-size)
-    (assoc entity :alive? live-state))
-
-(defn- convert-to-cell [[x y] live-state]
-  (assoc (texture
-           (if live-state "alive.jpeg"
-             "dead.jpeg")) :x x :y y :width pixel-size :height pixel-size :isCell? true :alive? live-state))
+  (texture! entity
+            :set-region (if live-state 0 pixel-size) 0 pixel-size pixel-size)
+  (assoc entity :alive? live-state))
 
 (defn find-neighbor-state [entities {:keys [x y] :as entity}]
-    (frequencies 
-      (map (fn [[neighbor-x neighbor-y]]
-             (cond
-               (or (< neighbor-x 0) (< neighbor-y 0)
-                   (> neighbor-x 200) (> neighbor-y 200)) false
-               :else 
-               (if (some 
-                     #(and (:isCell? %) (:alive? %) (== (:x %) neighbor-x) (== (:y %) neighbor-y))
-                     entities) true false))) (get neighbors (list x y)))))
+  (frequencies 
+    (map (fn [[neighbor-x neighbor-y]]
+           (cond
+             (or (< neighbor-x 0) (< neighbor-y 0)
+                 (> neighbor-x 200) (> neighbor-y 200)) false
+             :else 
+             (if (some 
+                   #(and (:isCell? %) (:alive? %) (== (:x %) neighbor-x) (== (:y %) neighbor-y))
+                   entities) true false))) (get neighbors (list x y)))))
 
 (defn- get-next-state [entities {:keys [alive?] :as entity}] 
   (let [neighbor-states (find-neighbor-state entities entity)
         alive-neighbors (get neighbor-states true 0)
         dead-neighbors  (get neighbor-states false 0)]
-        (cond
-          (and alive?
-               (or (< alive-neighbors 2) (> alive-neighbors 3))) false
-          (and alive?
-               (or (== alive-neighbors 2) (== alive-neighbors 3))) true
-          (and (not alive?) (== alive-neighbors 3)) true
-          :else alive?)))
+    (cond
+      (and alive?
+           (or (< alive-neighbors 2) (> alive-neighbors 3))) false
+      (and alive?
+           (or (== alive-neighbors 2) (== alive-neighbors 3))) true
+      (and (not alive?) (== alive-neighbors 3)) true
+      :else alive?)))
 
 (defn change-grid
   [entities]
   (loop [the-co-ordinates co-ordinates the-entities entities]
     (if (empty? the-co-ordinates)
       the-entities
-     (let [current-x-y (first the-co-ordinates) 
-           current-x (first current-x-y)
-           current-y (second current-x-y)
-           current-entity (first (filter #(and (= (:x %) current-x) (= (:y %) current-y)) the-entities))
-           the-index (.indexOf the-entities current-entity)
-           should-live (get-next-state the-entities current-entity)]
-       (recur (next the-co-ordinates) 
-              (assoc the-entities the-index (change-entity-state current-entity should-live)))))))
-       
+      (let [current-x-y (first the-co-ordinates) 
+            current-x (first current-x-y)
+            current-y (second current-x-y)
+            current-entity (first (filter #(and (= (:x %) current-x) (= (:y %) current-y)) the-entities))
+            the-index (.indexOf the-entities current-entity)
+            should-live (get-next-state the-entities current-entity)]
+        (recur (next the-co-ordinates) 
+               (assoc the-entities the-index (change-entity-state current-entity should-live)))))))
+
 (defscreen main-screen
   :on-show
   (fn [screen entities]
